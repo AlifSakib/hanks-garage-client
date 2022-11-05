bd064fc4de9291f4b4890351eae2e4c4091988b9
AlifSakib <alifsakib@gmail.com>
2022-11-05 14:55:21 +0600

    added services

---
+++ src/components/Pages/Home/Service.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Service = ({ service }) => {
+  const { title, img, price } = service;
+  return (
+    <div>
+      <div className="p-4 rounded shadow-md bg-slate-100">
+        <div className="h-full flex flex-col items-center text-start">
+          <img
+            alt="team"
+            className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+            src={img}
+          />
+          <div className="w-full">
+            <h2 className="title-font font-medium text-lg text-gray-900">
+              {title}
+            </h2>
+            <h3 className="text-gray-500 mb-3">{price}$</h3>
+            <p className="mb-4">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+              debitis blanditiis beatae quidem nobis molestiae consectetur
+              officia eaque ducimus voluptatum!
+            </p>
+            <button className="px-6 py-2 font-bold tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80">
+              Take Service
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -4,50 +4,50 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
             to="/"
-            class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-10 h-10 text-black p-2 bg-red-500 rounded-full"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-10 h-10 text-black p-2 bg-red-500 rounded-full"
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl font-bold">
+            <span className="ml-3 text-xl font-bold">
               Hank's <span className="text-red-600">Garage</span>
             </span>
           </Link>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center font-bold">
-            <Link to="/home" class="mr-5 hover:text-gray-900">
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-bold">
+            <Link to="/home" className="mr-5 hover:text-gray-900">
               Home
             </Link>
-            <Link to="/orders" class="mr-5 hover:text-gray-900">
+            <Link to="/orders" className="mr-5 hover:text-gray-900">
               Order
             </Link>
-            <a class="mr-5 hover:text-gray-900">Third Link</a>
-            <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+            <Link className="mr-5 hover:text-gray-900">Third Link</Link>
+            <Link className="mr-5 hover:text-gray-900">Fourth Link</Link>
           </nav>
           <Link
             to="/signup"
-            class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+            className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
           >
             Signup
             <svg
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
               viewBox="0 0 24 24"
             >
               <path d="M5 12h14M12 5l7 7-7 7"></path>
