e9dae3ab4e737e9ec6f6439ba37e6a12bf1521c0
AlifSakib <alifsakib@gmail.com>
2022-11-07 14:11:44 +0600

    modifyed hero section

--- src/components/Pages/Home/Hero.js
+++ src/components/Pages/Home/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import hero from "../../../assets/hero.jpg";
+import hero from "../../../assets/hero.avif";
 const Hero = () => {
   return (
     <div>
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
             <img
-              className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+              className="lg:w-4/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
               alt="hero"
               src={hero}
             />
@@ -16,18 +16,16 @@ const Hero = () => {
                 Bulletin boards are sort of the garage bands of cyberspace.
               </h1>
               <p className="mb-8 leading-relaxed">
-                Meggings kinfolk echo park stumptown DIY, kale chips beard
-                jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
-                godard disrupt ramps hexagon mustache umami snackwave tilde
-                chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui
-                celiac mlkshk freegan photo booth af fingerstache pitchfork.
+                An automobile repair shop (also known regionally as a garage or
+                a workshop) is an establishment where automobiles are repaired
+                by auto mechanics and technicians.
               </p>
               <div className="flex justify-center">
-                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  Button
+                <button className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+                  Explore
                 </button>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                  Button
+                <button className="ml-4 inline-flex bg-slate-800 border-0 py-2 px-6 focus:outline-none hover:bg-slate-900 text-white rounded text-lg">
+                  Order
                 </button>
               </div>
             </div>
