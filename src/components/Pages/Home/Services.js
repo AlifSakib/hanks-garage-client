3c0afccecd184ee91280534a302df813f503394d
AlifSakib <alifsakib@gmail.com>
2022-11-07 13:13:20 +0600

    added pagination

--- src/components/Pages/Home/Services.js
+++ src/components/Pages/Home/Services.js
@@ -3,11 +3,19 @@ import Service from "./Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(3);
+
+  const pages = Math.ceil(count / size);
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(`http://localhost:5000/services?page=${page}&size=${size}`)
       .then((res) => res.json())
-      .then((data) => setServices(data.data));
-  }, []);
+      .then((data) => {
+        setServices(data.data.services);
+        setCount(data.data.count);
+      });
+  }, [page, size]);
   return (
     <div>
       <section className="text-gray-600 body-font">
@@ -26,6 +34,28 @@ const Services = () => {
             {services.map((service) => (
               <Service key={service._id} service={service}></Service>
             ))}
+          </div>
+          <div>
+            {[...Array(pages).keys()].map((number) => (
+              <button
+                onClick={() => setPage(number)}
+                className="mx-5 my-10 px-4 bg-red-500 py-1 text-white rounded-md"
+                key={number}
+              >
+                {number + 1}
+              </button>
+            ))}
+            <select
+              className="mx-5 my-10 px-4 bg-red-500 py-1 text-white rounded-md"
+              onChange={(e) => setSize(e.target.value)}
+            >
+              <option value="3" selected>
+                3
+              </option>
+              <option value="6">6</option>
+              <option value="9">9</option>
+              <option value="12">12</option>
+            </select>
           </div>
         </div>
       </section>
